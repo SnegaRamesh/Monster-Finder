5fc8fed8547e9d3709a71d361d99f8ced44a6044
Sowmiya <sowmiyathilakavathi@gmail.com>
2020-01-26 16:35:08 +0530

    Updates

---
+++ precache-manifest.308d76a5e3cad405dcb7c75643a56228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d37435514fcd2998b533faa24a5336",
+    "url": "/Monster-Finder/index.html"
+  },
+  {
+    "revision": "6ef0a8f47e7d921394a0",
+    "url": "/Monster-Finder/static/css/main.933e17e0.chunk.css"
+  },
+  {
+    "revision": "508c46ae83c99feaae35",
+    "url": "/Monster-Finder/static/js/2.4b299025.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monster-Finder/static/js/2.4b299025.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ef0a8f47e7d921394a0",
+    "url": "/Monster-Finder/static/js/main.912796d8.chunk.js"
+  },
+  {
+    "revision": "081b3c42aede594d9454",
+    "url": "/Monster-Finder/static/js/runtime-main.9f97ac02.js"
+  }
+]);
